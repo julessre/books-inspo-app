d365120a6aacf7175575eece542f5fcf23739176
Julia <jules.rechi@gmail.com>
2024-02-28 16:28:56 +0100

    Adapt navigation bar

--- books-inspo/navigation/page.jsx
+++ books-inspo/navigation/page.jsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { colors } from '../styles/constants';
 // Screens
@@ -17,23 +18,45 @@ const Tab = createBottomTabNavigator();
 
 const screenOptions = ({ route }) => ({
   tabBarIcon: ({ focused, color, size }) => {
+    // <View
+    //   style={{
+    //     backgroundColor: colors.primaryColor,
+    //     borderRadius: 20,
+    //     padding: 10,
+    //     shadowColor: 'black',
+    //     shadowOpacity: 0.5,
+    //     shadowOffset: { width: 0, height: 2 },
+    //     shadowRadius: 2,
+    //     elevation: 5, // for Android
+    //   }}
+    // />;
     let iconName;
 
     if (route.name === homeName) {
-      iconName = focused ? 'book' : 'book-outline';
+      iconName = focused ? 'book' : 'book';
     } else if (route.name === favName) {
-      iconName = focused ? 'heart' : 'heart-outline';
+      iconName = focused ? 'heart' : 'heart';
     } else if (route.name === profileName) {
-      iconName = focused ? 'person' : 'person-outline';
+      iconName = focused ? 'person' : 'person';
     }
 
-    return <Ionicons name={iconName} size={size} color={color} />;
+    return <Ionicons name={iconName} size={size + 5} color={color} />;
   },
 
   tabBarActiveTintColor: colors.primaryColor,
   tabBarInactiveTintColor: colors.text,
-  tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
-  tabBarStyle: { padding: 30, height: 70 },
+  tabBarLabelStyle: {
+    paddingBottom: 10,
+    display: 'none',
+  },
+  tabBarStyle: {
+    padding: 20,
+    height: 90,
+    backgroundColor: colors.background,
+  },
+  tabBarOptions: {
+    showLabel: false,
+  },
 });
 
 export default function Navigation() {
