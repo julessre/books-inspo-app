b387a164fdf1e9725b601db57850cbdd55e4ca1e
Julia <jules.rechi@gmail.com>
2024-02-28 17:25:41 +0100

    Add background color to pages

--- books-inspo/navigation/screens/HomeScreen.jsx
+++ books-inspo/navigation/screens/HomeScreen.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../../styles/constants';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: colors.text,
+  },
+});
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text onPress={() => alert('This is the Home Screen.')}>Home Screen</Text>
+    <View style={styles.container}>
+      <Text
+        style={styles.text}
+        onPress={() => alert('This is the Home Screen.')}
+      >
+        Home Screen
+      </Text>
     </View>
   );
 }
--- books-inspo/navigation/screens/ProfileScreen.jsx
+++ books-inspo/navigation/screens/ProfileScreen.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../../styles/constants';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: colors.text,
+  },
+});
 
 export default function ProfileScreen({ navigation }) {
   return (
-    <View>
-      <Text onPress={() => navigation.navigate('Home')}>Profile Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text} onPress={() => navigation.navigate('Home')}>
+        Profile Screen
+      </Text>
     </View>
   );
 }
